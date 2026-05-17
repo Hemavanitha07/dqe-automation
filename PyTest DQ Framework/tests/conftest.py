ed80dfb5d9097cf228e87c9e6e07dd3c3009ac1d
Hemavanitha07 <Hemavanitha_velliangiri@epam.com>
2026-05-17 21:10:55 +0530

    Update conftest.py

--- PyTest DQ Framework/tests/conftest.py
+++ PyTest DQ Framework/tests/conftest.py
@@ -1,29 +1,48 @@
 import pytest
-from src.connectors.postgres.postgres_connector import PostgresConnectorContextManager
-from src.data_quality.data_quality_validation_library import DataQualityLibrary
+
 from src.connectors.file_system.parquet_reader import ParquetReader
+from src.connectors.postgres.postgres_connector import (
+    PostgresConnectorContextManager
+)
+from src.data_quality.data_quality_validation_library import (
+    DataQualityLibrary
+)
+
 
 def pytest_addoption(parser):
-    parser.addoption("--db_host", action="store", default="localhost", help="Database host")
 
-def pytest_configure(config):
-    """
-    Validates that all required command-line options are provided.
-    """
-    required_options = [
-        "--db_user", "--db_password"
-    ]
-    for option in required_options:
-        if not config.getoption(option):
-            pytest.fail(f"Missing required option: {option}")
+    parser.addoption("--db_host", action="store", default="localhost")
+    parser.addoption("--db_port", action="store", default="5432")
+    parser.addoption("--db_name", action="store", default="mydatabase")
+    parser.addoption("--db_user", action="store")
+    parser.addoption("--db_password", action="store")
+
 
 @pytest.fixture(scope='session')
 def db_connection(request):
-    ...
-    try:
-        with PostgresConnectorContextManager(...) as db_connector:
-            yield db_connector
-    except Exception as e:
-        pytest.fail(f"Failed to initialize PostgresConnectorContextManager: {e}")
 
-...
+    db_host = request.config.getoption("--db_host")
+    db_port = request.config.getoption("--db_port")
+    db_name = request.config.getoption("--db_name")
+    db_user = request.config.getoption("--db_user")
+    db_password = request.config.getoption("--db_password")
+
+    with PostgresConnectorContextManager(
+        db_user=db_user,
+        db_password=db_password,
+        db_host=db_host,
+        db_name=db_name,
+        db_port=db_port
+    ) as db_connector:
+
+        yield db_connector
+
+
+@pytest.fixture(scope='session')
+def parquet_reader():
+    yield ParquetReader()
+
+
+@pytest.fixture(scope='session')
+def data_quality_library():
+    yield DataQualityLibrary()
